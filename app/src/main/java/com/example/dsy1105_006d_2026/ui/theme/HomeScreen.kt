5b55a20aa7f22a67ee757b2e38c4144dc5dde5fb
Duoc <vice.zapata@profesor.duoc.cl>
2026-09-21 09:55:16 -0300

    primer 1

--- app/src/main/java/com/example/dsy1105_006d_2026/ui/theme/HomeScreen.kt
+++ app/src/main/java/com/example/dsy1105_006d_2026/ui/theme/HomeScreen.kt
@@ -1,21 +1,31 @@
 package com.example.dsy1105_006d_2026.ui.theme
 
+
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.dsy1105_006d_2026.R
@@ -24,10 +34,23 @@ import com.example.dsy1105_006d_2026.R
 @Composable
 
 fun HomeScreen(){
+    //dark color que es una funcion del MaterialTheme3
+    val ColorScheme= darkColorScheme(
+        primary= Color(0xFF98222E),
+        onPrimary = Color.White,
+        onSurface = Color(0xFF333333),
+    )// fin ColorScheme
+
+    MaterialTheme(
+        colorScheme=ColorScheme
+    ){ //inicio Material
+
     //Genera una pantalla basica
     Scaffold(
       topBar ={
-          TopAppBar({Text("Mi App en Kptlin")})
+          TopAppBar({Text("Mi App en Kotlin",
+              color= MaterialTheme.colorScheme.onPrimary
+          )})
       }  // fin top
 
     ) // fin Scaff
@@ -37,13 +60,19 @@ fun HomeScreen(){
             modifier= Modifier
                 .padding(innerPadding)
                 .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(20.dp)
+                .padding(16.dp)
+                .background(Color(0xFFF0F0F0)),
+
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+
         ) {
-            Text("!!!!  Bienvenido !!!!!")
-            Button(onClick ={/*Accion futura*/ } ) {
-                Text ("Presoname")
-            }
+            Text("!!!!  Bienvenido !!!!!",
+                style= MaterialTheme.typography.headlineMedium,
+                color= MaterialTheme.colorScheme.primary
+
+            )
+            Spacer(modifier=Modifier.height(66.dp))
 
             Image(
                 painter = painterResource(id=R.drawable.logoduoc),
@@ -54,13 +83,52 @@ fun HomeScreen(){
                 contentScale = ContentScale.Fit
             )
 
+            Spacer(modifier=Modifier.height(66.dp))
+
+            Row(
+                modifier= Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal=16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+
+            )//fon row
+            {//inicio aplicacion
+
+                Text("texto uno",
+                    style= MaterialTheme.typography.bodyLarge.copy(
+                        color= MaterialTheme.colorScheme.onSurface.copy(alpha=0.8f),
+                        fontWeight = FontWeight.Bold),
+                    modifier= Modifier
+                        .padding(end=8.dp)
+                )//Fin texto 1
+
+
+                Text("texto dos",
+                    style= MaterialTheme.typography.bodyLarge.copy(
+                        color= MaterialTheme.colorScheme.onSurface.copy(alpha=0.8f),
+                        fontWeight = FontWeight.Bold),
+                    modifier= Modifier
+                        .padding(end=8.dp)
+                )//Fin texto 2
+
+            }//fin aplicacion
+
+            Spacer(modifier=Modifier.height(66.dp))
+
+            Button(onClick ={/*Accion futura*/ },
+                modifier= Modifier
+                    .fillMaxWidth(0.8f)
+
+            ) {
+                Text ("Presoname")
+            }
 
         }// fin column
 
 
 
     }// fin inner
-
+    }//fin Material
 }// fin HomeScreen
 
 
